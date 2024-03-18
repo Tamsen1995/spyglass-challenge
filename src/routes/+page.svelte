46ddbfa96dafd034c62f9f2c1eeccd02ff0cb588
Tamsen1995 <tamanhbui95@gmail.com>
2024-03-18 11:39:36 -0400

    Add some very basic styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,8 +44,52 @@
 </script>
 
 <style>
+    main {
+        background-color: #333;
+        color: #fff;
+        font-family: 'Helvetica Neue', sans-serif;
+        padding: 2em;
+    }
+
+    h1 {
+        font-size: 2.5em;
+        margin-bottom: 1em;
+    }
+
+    p {
+        font-size: 1.2em;
+        line-height: 1.5;
+        margin-bottom: 1em;
+    }
+
+    a {
+        color: #ff0;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #ff0;
+        text-decoration: underline;
+    }
+
+    button {
+        background-color: #555;
+        border: none;
+        color: #fff;
+        cursor: pointer;
+        font-size: 1em;
+        margin: 0.5em;
+        padding: 0.5em 1em;
+    }
+
+    button:disabled {
+        background-color: #777;
+        cursor: not-allowed;
+    }
+
     .active {
-        color: red;
+        background-color: #ff0;
+        color: #000;
     }
 </style>
   
--- src/routes/planet/+page.svelte
+++ src/routes/planet/+page.svelte
@@ -20,6 +20,56 @@
     }
 </script>
 
+<style>
+    main {
+        background-color: #333;
+        color: #fff;
+        font-family: 'Helvetica Neue', sans-serif;
+        padding: 2em;
+    }
+
+    h1 {
+        font-size: 2.5em;
+        margin-bottom: 1em;
+    }
+
+    p {
+        font-size: 1.2em;
+        line-height: 1.5;
+        margin-bottom: 1em;
+    }
+
+    a {
+        color: #ff0;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #ff0;
+        text-decoration: underline;
+    }
+
+    button {
+        background-color: #555;
+        border: none;
+        color: #fff;
+        cursor: pointer;
+        font-size: 1em;
+        margin: 0.5em;
+        padding: 0.5em 1em;
+    }
+
+    button:disabled {
+        background-color: #777;
+        cursor: not-allowed;
+    }
+
+    .active {
+        background-color: #ff0;
+        color: #000;
+    }
+</style>
+
 <main>
     {#if planet}
         <h1>{planet.name}</h1>

70f350a02e0dc0106246f1b70fc7aa89a10eeec2
Tamsen1995 <tamanhbui95@gmail.com>
2024-03-17 20:10:14 -0400

    make page numbers specific

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,8 @@
         let next: string | null = null;
         let previous: string | null = null;
         let page: number = 1;
+        const maxPage = 6; // Set this to the maximum number of pages you expect
+        const pages = Array.from({length: maxPage}, (_, i) => i + 1);
     
         async function loadPlanets(url: string | null) {
             try {
@@ -33,6 +35,11 @@
             }
         }
     
+        function goToPage(p: number) {
+            loadPlanets(`https://swapi.dev/api/planets/?page=${p}`);
+            page = p;
+        }
+    
         onMount(() => loadPlanets('https://swapi.dev/api/planets/'));
 </script>
     
@@ -43,5 +50,8 @@
         <p>{planet.name}</p>
     {/each}
     <button on:click={previousPage} disabled={!previous}>Previous</button>
+    {#each pages as p}
+        <button on:click={() => goToPage(p)}>{p}</button>
+    {/each}
     <button on:click={nextPage} disabled={!next}>Next</button>
 </main>
